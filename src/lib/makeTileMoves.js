3c3cf702499a246bd661ad52040f2a414187daec
Mark Powell <hi@powellmark.com>
2018-05-25 21:23:55 +1000

    created lib file for game logic. Tiles are still not moving yet

---
+++ src/lib/makeTileMoves.js
@@ -0,0 +1,8 @@
+export default function makeMoves(blankTile, tiles) {
+    console.log('Board created');
+
+    // get current coordinate of blank tile from the hardcoded array
+    let blankTileX = 3;
+    let blankTileY = 3;
+
+}
